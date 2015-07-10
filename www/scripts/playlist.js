42a8ad88a80e43896486682b49f3233fb45507da
orangesheep <therealjmd3652@gmail.com>
2015-07-09 22:35:03 -0500

    yes

---
+++ www/scripts/playlist.js
@@ -0,0 +1,17 @@
+trackList = [
+   {
+    name:"We Chill All Night Remastered",
+    artist:"TeknoAXE ",
+    src:"music/We_Chill_All_Night_Remastered.mp3",
+	img:"music/tekno.png",
+	comment:"http://teknoaxe.com/Link_Code_3.php?q=876"
+  }, 
+  {
+    name:"orsh mix 1",
+    artist:"orsh",
+    src:"music/orsh_mix_1.mp3",
+	img:"music/i swear he doesnt bite.png",
+	comment:""
+  } 
+ 
+];
